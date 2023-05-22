6dc7bab682d5c5458ed483b040376407231f9d87
Eric Silberstein <ericsilberstein1@gmail.com>
2023-05-22 18:45:56 +0000

    Internationalize index.js

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,9 @@ const Home = () => {
   return (
     <div suppressHydrationWarning>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <title>
+          Nextly - Free Nextjs & TailwindCSS Landing Page Template
+        </title>
         <meta
           name="description"
           content="Nextly is a free landing page template built with next.js & Tailwind CSS"
@@ -53,37 +55,30 @@ const Home = () => {
       </Head>
 
       <Navbar />
-      <h1>
-        {i18next.t('test_title')}
-      </h1>
       <Hero />
       <SectionTitle
-        pretitle="Nextly Benefits"
-        title=" Why should you use this landing page">
-        ANIL ERIC Nextly is a free landing page & marketing website template for startups
-        and indie projects. Its built with Next.js & TailwindCSS. And its
-        completely open-source.
+        pretitle={t('index_benefits_pretitle')}
+        title={t('index_benefits_title')}>
+        {t('index_benefits_bodyDescription')}
       </SectionTitle>
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
       <SectionTitle
-        pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
-        This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
-        don&apos;t forget to add one. Just like this.
+        pretitle= {t('index_video_pretitle')}
+        title={t('index_video_title')}>
+        {t('index_video_bodyDescription')}
       </SectionTitle>
       <Video />
       <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said">
-        Testimonails is a great way to increase the brand trust and awareness.
-        Use this section to highlight your popular customers.
+        pretitle={t('index_testimonials_pretitle')}
+        title={t('index_testimonials_title')}>
+        {t('index_testimonials_bodyDescription')}
       </SectionTitle>
       <Testimonials />
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
+      <SectionTitle 
+        pretitle={t('index_faq_pretitle')}
+        title={t('index_faq_title')}>
+        {t('index_faq_bodyDescription')}
       </SectionTitle>
       <Faq />
       <Cta />
