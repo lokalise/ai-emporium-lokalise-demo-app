7b350bc58de00d41273cc3486428cb82ecc59565
silbersteine <silbersteine@users.noreply.github.com>
2024-02-09 03:23:20 +0000

    Revert to 0ba3c47

--- components/hero.js
+++ components/hero.js
@@ -12,11 +12,9 @@ const Hero = () => {
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
               {t('hero_header_websiteDescription')}
-              {t('hello-twin-health')}
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               {t('hero_body_description')}
-              A second string
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
